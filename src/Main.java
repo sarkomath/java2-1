38b0fd68f6422da6c727317adfc6c0f95e3802a8
Nikita Novoselov <sarkomath@outlook.com>
2020-11-05 20:28:16 +1000

    report.md compl.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int client = 2_000_000_000;
-        int transaction = 500_000_000;
+        int client = 6;
+        int transaction = 5;
         int total = client + transaction;
         System.out.println("total is:");
         System.out.println(total);
