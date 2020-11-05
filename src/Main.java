5a6e65653b8bfd9cdd2f69bdb2b4def92ed51e7b
Nikita Novoselov <sarkomath@outlook.com>
2020-11-05 18:37:41 +1000

    add report.md

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int client = 2_000_000_000;
+        int transaction = 500_000_000;
+        int total = client + transaction;
+        System.out.println("total is:");
+        System.out.println(total);
+    }
+}
